e3cd6d84577fb744e1e4ecbf766736bc96275342
Tony Tiratay <tony.tiratay@gmail.com>
2017-02-14 21:23:28 +0100

    Add routers, layouts and container

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -4,10 +4,11 @@ import {mount} from 'react-mounter';
 
 import {MainLayout} from '../layouts/MainLayout.js';
 import Home from '../pages/Home.js';
+import Menu from '../components/Home.js';
 
- FlowRouter.route('/', {
-   name: 'home',
-   action: function() {
-     mount(MainLayout, {content: <Home />});
-   }
- });
+FlowRouter.route('/', {
+  name: 'home',
+  action: function () {
+    mount(MainLayout, { content: <Home /> });
+  },
+});
