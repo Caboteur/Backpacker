86d2d48260404763bf5dc84cad00f387e5cc9d0b
Tony Tiratay <tony.tiratay@gmail.com>
2017-02-14 18:28:54 +0100

    Add github request and state to container

--- imports/pages/Home.js
+++ imports/pages/Home.js
@@ -1,6 +1,30 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class Home extends Component {
+  constructor(){
+    super()
+    this.state = {
+      term: 'meteor',
+      data: []
+    };
+  }
+
+  componentWillMount(){
+    this.getData(this.state.term);
+  }
+
+  getData(term) {
+    const root = 'https://api.github.com/search/repositories?q=';
+    axios.get( root + term)
+      .then((response) => {
+        this.setState({data: response.data.items});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render(){
     return (
       <div className="">
