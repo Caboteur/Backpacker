4091c65289d7da1d2b80b9a51e5fffb91ed0482e
Tony Tiratay <tony.tiratay@gmail.com>
2017-02-28 20:27:00 +0100

    Redo of s4

--- server/test.js
+++ server/test.js
@@ -1,13 +1,11 @@
 
 const Articles = new Mongo.Collection("articles");
 
-console.log(Meteor.users.find().fetch());
-
 Meteor.methods({
   listeArticles: ()=>{
     // throw new Meteor.Error(500, "Désolé nous n'avons pas réussi à récupérer les articles");
+    console.log(Meteor.user());
     return Articles.find().fetch();
-    console.log(Meteor.userId)
   },
   getArticle: (id)=>{
     return Articles.findOne({_id: id});
